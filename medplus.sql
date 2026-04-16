9691e0300ddc99c73e588c4068d4924445b27f28
varshinirvarsha <varshinirvarsha3@gmail.com>
2026-04-16 17:05:30 +0530

    16/4/26 tables

---
+++ medplus.sql
@@ -0,0 +1,13 @@
+create table medplus;
+use medplus;
+
+create table medplus (
+    medplus_id CHAR(4) PRIMARY KEY,
+    userName VARCHAR(20) NOT NULL UNIQUE,
+    password VARCHAR(20) CHECK (LENGTH(password) > 8),
+    email VARCHAR(30) CHECK (email LIKE '%@gmail.com'),
+    phone_number BIGINT UNIQUE,
+    dob DATE NOT NULL CHECK (dob BETWEEN '2000-01-01' AND '2015-12-31'),
+    age INT CHECK (age > 10),
+    country VARCHAR(10) DEFAULT 'India'
+);
---
+++ paytm.sql
@@ -0,0 +1,14 @@
+create database paytm_user;
+use paytm_user;
+
+
+create table paytm_user (
+    paytm_id CHAR(4) PRIMARY KEY,
+    userName VARCHAR(20) NOT NULL UNIQUE,
+    password VARCHAR(20) CHECK (LENGTH(password) > 8),
+    email VARCHAR(30) CHECK (email LIKE '%@gmail.com'),
+    phone_number BIGINT UNIQUE,
+    dob DATE NOT NULL CHECK (dob BETWEEN '2000-01-01' AND '2015-12-31'),
+    age INT CHECK (age > 10),
+    country VARCHAR(10) DEFAULT 'India'
+);
